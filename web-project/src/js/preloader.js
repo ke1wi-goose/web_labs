8501affb28349471809fbc0a39c418802e1e6823
Oleksandr Nychyk <nychykoleksandr@gmail.com>
2024-10-03 19:11:22 +0300

    hotfix

--- web-project/src/js/preloader.js
+++ web-project/src/js/preloader.js
@@ -1,4 +1,8 @@
 window.addEventListener("load", function () {
     const preloader = document.getElementById("preloader");
-    preloader.style.display = "none";
-  });
+
+    // Додаємо невелику затримку перед видаленням preloader
+    setTimeout(function () {
+      preloader.style.display = "none";
+    }, 350);
+});
